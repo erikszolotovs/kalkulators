b05597b91fdcf816296b3f0bdd77f5eb21e339f3
erikszolotovs <145644412+erikszolotovs@users.noreply.github.com>
2024-05-16 09:55:59 +0300

    otra stunda

--- kalkulators.py
+++ kalkulators.py
@@ -8,14 +8,15 @@ def btnClick(number):
     e.delete(0,END)#nodzēš
     newNumber=str(current)+str(number)
     e.insert(0,newNumber)#ievieto displejā jauno skaitli
-    return 0
+    return
 
 
 def btnCommand(command):
     global number
     global num1 #jāiegaumē skaitlis,darbība
     global mathOp #matemātiskais operators
-    mathOp=commandnum1=int(e.get())
+    mathOp=command
+    num1=int(e.get())
     e.delete(0,END)
     return 0
 
@@ -48,22 +49,22 @@ def notirit():
 e=Entry(mansLogs,width=15,bd=10,font=("Arial Black",20),justify="right") #izveido ievades logu
 e.grid(row=0,column=0,columnspan=4)#atrašanās vieta 
 
-btn0=Button(mansLogs,text='0',padx='40',pady='20',command=lambda:btnClick(0))
-btn1=Button(mansLogs,text='1',padx='40',pady='20',command=lambda:btnClick(1))
-btn2=Button(mansLogs,text='2',padx='40',pady='20'command=lambda:btnClick(2))
-btn3=Button(mansLogs,text='3',padx='40',pady='20'command=lambda:btnClick(3))
-btn4=Button(mansLogs,text='4',padx='40',pady='20'command=lambda:btnClick(4))
-btn5=Button(mansLogs,text='5',padx='40',pady='20'command=lambda:btnClick(5))
-btn6=Button(mansLogs,text='6',padx='40',pady='20'command=lambda:btnClick(6))
-btn7=Button(mansLogs,text='7',padx='40',pady='20'command=lambda:btnClick(7))
-btn8=Button(mansLogs,text='8',padx='40',pady='20'command=lambda:btnClick(8))
-btn9=Button(mansLogs,text='9',padx='40',pady='20'command=lambda:btnClick(9))
-btnC=Button(mansLogs,text='C',padx='40',pady='20'command=notirit)
-btnPlus=Button(mansLogs,text='+',padx='40',pady='20'command=lambda:btnCommand('+'))
-btnMinus=Button(mansLogs,text='-',padx='40',pady='20'command=lambda:btnCommand('-'))
-btnreizinasana=Button(mansLogs,text='*',padx='40',pady='20'command=lambda:btnCommand('*'))
-btndalisana=Button(mansLogs,text='/',padx='40',pady='20'command=lambda:btnCommand('/'))
-btnvienads=Button(mansLogs,text='=',padx='40',pady='20'  command=Vienads)
+btn0=Button(mansLogs,text='0',padx='40',pady='20',command=lambda:btnClick(0),bd = 10,bg='blue',font=('Arial Black',20))
+btn1=Button(mansLogs,text='1',padx='40',pady='20',command=lambda:btnClick(1),bd = 10,bg='blue',font=('Arial Black',20))
+btn2=Button(mansLogs,text='2',padx='40',pady='20',command=lambda:btnClick(2),bd = 10,bg='blue',font=('Arial Black',20))
+btn3=Button(mansLogs,text='3',padx='40',pady='20',command=lambda:btnClick(3),bd = 10,bg='blue',font=('Arial Black',20))
+btn4=Button(mansLogs,text='4',padx='40',pady='20',command=lambda:btnClick(4),bd = 10,bg='blue',font=('Arial Black',20))
+btn5=Button(mansLogs,text='5',padx='40',pady='20',command=lambda:btnClick(5),bd = 10,bg='blue',font=('Arial Black',20))
+btn6=Button(mansLogs,text='6',padx='40',pady='20',command=lambda:btnClick(6),bd = 10,bg='blue',font=('Arial Black',20))
+btn7=Button(mansLogs,text='7',padx='40',pady='20',command=lambda:btnClick(7),bd = 10,bg='blue',font=('Arial Black',20))
+btn8=Button(mansLogs,text='8',padx='40',pady='20',command=lambda:btnClick(8),bd = 10,bg='blue',font=('Arial Black',20))
+btn9=Button(mansLogs,text='9',padx='40',pady='20',command=lambda:btnClick(9),bd = 10,bg='blue',font=('Arial Black',20))
+btnC=Button(mansLogs,text='C',padx='40',pady='20',command=notirit,bd = 10,bg='red',font=('Arial Black',20))
+btnPlus=Button(mansLogs,text='+',padx='40',pady='20',command=lambda:btnCommand('+'),bd = 10,bg='blue',font=('Arial Black',20))
+btnMinus=Button(mansLogs,text='-',padx='40',pady='20',command=lambda:btnCommand('-'),bd = 10,bg='blue',font=('Arial Black',20))
+btnreizinasana=Button(mansLogs,text='*',padx='40',pady='20',command=lambda:btnCommand('*'),bd = 10,bg='blue',font=('Arial Black',20))
+btndalisana=Button(mansLogs,text='/',padx='40',pady='20',command=lambda:btnCommand('/'),bd = 10,bg='blue',font=('Arial Black',20))
+btnvienads=Button(mansLogs,text='=',padx='40',pady='20',command=Vienads,bd = 10,bg='blue',font=('Arial Black',20))
 
 
 btn7.grid(row=1,column=0)
